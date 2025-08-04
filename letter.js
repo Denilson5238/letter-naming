178e31d9b2a7a26c2c37339a1ec11baf777e9528
Denilson5238 <denilsonst27@gmail.com>
2025-08-04 18:39:28 +0000

    revert back to previous changes

--- letter.js
+++ letter.js
@@ -3,7 +3,6 @@ var firstChar = name.slice(0,1);
 var upperCasefirstChar = firstChar.toUpperCase();
 var upperCase = name.toUpperCase();
 var restOfname = name.slice(1,name.length);
-// test
 var restOfname = restOfname.toLowerCase();
 var capitalisedName = upperCasefirstChar + restOfname;
-console.log("Hello, " + capitalisedName)
+console.log("Hello, " + capitalisedName);
